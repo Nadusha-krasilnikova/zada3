91fa32f3cfe2eb3a68a58089a9ae4ce5e711900d
Nadusha <nadusha230275@gmail.com>
2022-07-03 17:24:20 +0500

    закомитела все папки

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// найти максимальное из трех чисел
+Console.WriteLine("введите три числа");
+int a=int.Parse(Console.ReadLine());
+int b=int.Parse(Console.ReadLine());
+int c=int.Parse(Console.ReadLine());
+int max=a;
+if (a>max) max=a;
+if (b>max) max=b;
+if (c>max) max=c;
+Console.WriteLine("максимальное число ");
+Console.Write (max);
